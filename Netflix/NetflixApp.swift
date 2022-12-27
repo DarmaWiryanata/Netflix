10ff260e556dc3b1c44460cf4fa52bae6da5d8f1
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-12-27 21:58:42 +0800

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Darma Wiryanata on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
