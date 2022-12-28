90e5cc664398deb1160f111cd457451d810684e3
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-12-28 10:18:18 +0800

    Movie & video models

---
+++ Netflix/Models/Movie.swift
@@ -0,0 +1,39 @@
+//
+//  Movie.swift
+//  Netflix
+//
+//  Created by Darma Wiryanata on 28/12/22.
+//
+
+import Foundation
+
+struct Movie: Identifiable, Codable {
+    let id: Int
+    let adult: Bool
+    let backdropPath: String
+    let genreIDS: [Int]
+    let originalLanguage, originalTitle, overview: String
+    let popularity: Double
+    let posterPath, releaseDate, title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id, adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+struct MoviesResults: Codable {
+    let results: [Movie]
+}
---
+++ Netflix/Models/Video.swift
@@ -0,0 +1,28 @@
+//
+//  Video.swift
+//  Netflix
+//
+//  Created by Darma Wiryanata on 28/12/22.
+//
+
+import Foundation
+
+struct Video: Identifiable, Codable {
+    let id, iso6391, iso31661, name, key, site: String
+    let size: Int
+    let type: String
+    let official: Bool
+    let publishedAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case iso6391 = "iso_639_1"
+        case iso31661 = "iso_3166_1"
+        case name, key, site, size, type, official
+        case publishedAt = "published_at"
+    }
+}
+
+struct VideosResults: Codable {
+    let results: [Video]
+}
