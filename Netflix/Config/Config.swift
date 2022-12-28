7adc29d9ae65bcb441447af19be531f6fe748bf5
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-12-28 10:21:10 +0800

    TMDB configuration

---
+++ Netflix/Config/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  Netflix
+//
+//  Created by Darma Wiryanata on 28/12/22.
+//
+
+import Foundation
+
+struct Config {
+    static let tmdbBaseUrl = "https://api.themoviedb.org/3"
+    static let tmdbApiKey = "eeb90e5bbc4a740b97cc7a4d5a610f06"
+}
